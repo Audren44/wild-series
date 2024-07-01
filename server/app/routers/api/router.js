7c53ba33f0f35f7c412ffb9e915e563b24b75c51
Audren <audren.destin@gmail.com>
2024-07-01 22:58:46 +0200

    quête 3 harmonia

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,13 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
 
 const itemsRouter = require("./items/router");
+const programRouter = require("./programs/router");
 
 router.use("/items", itemsRouter);
+
+router.use("/programs", programRouter);
 
 /* ************************************************************************* */
 
